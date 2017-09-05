75612896e292b10736996ba3224ead53beb501ef
Paul Tomchik <pauljtomchik@live.com>
2017-09-05 17:07:51 -0400

    Renamed relay-integration dir to avoid conflicts with branch name... doh!

---
+++ relay-integration/installDependencies.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const {
+  execSync
+} = require('child_process')
+
+execSync('yarn add relay-runtime react-relay')
+execSync('yarn add relay-compiler babel-plugin-relay --dev')
