4223fbad68b1d496b0019b9feb81bb1ce1c9265f
Paul Tomchik <pauljtomchik@live.com>
2017-09-05 18:29:18 -0400

    relay-integration

---
+++ relay-integration/SCHEMA_URL.js
@@ -0,0 +1,7 @@
+// This should be moved into a central config file.
+
+const HOST = 'http://localhost';
+const PORT = ':4000';
+const PATH = '/postgraphql.trafficData.graphql';
+
+module.exports = `${HOST}${PORT}${PATH}`;
