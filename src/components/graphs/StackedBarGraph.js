09fd61114a037388b3cfd4f02028cebba44ebc35
alex <am3081@gmail.com>
2017-09-11 17:26:54 -0400

    showing counts

---
+++ src/components/graphs/StackedBarGraph.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  VictoryChart,
+  VictoryLegend,
+  VictoryAxis,
+  VictoryGroup,
+  VictoryStack,
+  VictoryBar
+} from 'victory';
+
+class StackedBarGraph extends React.Component {
+  render() {
+    return (
+      <div>
+        <VictoryChart
+          padding={{ right: 0, top: 10, left: 40, bottom: 50 }}
+          domainPadding={{ x: 10 }}
+        >
+          <VictoryLegend
+            colorScale={'red'}
+            x={40}
+            y={20}
+            gutter={0}
+            data={this.props.legendData}
+            style={{
+              labels: { fontSize: 12 },
+              parent: { border: '1px solid #ccc' }
+            }}
+          />
+          <VictoryAxis
+            label="Time (hrs)"
+            tickCount={12}
+            style={{
+              axisLabel: { fontSize: 12, padding: 25 },
+              ticks: { stroke: 'grey', size: 5 },
+              tickLabels: { fontSize: 8, padding: 5 }
+            }}
+          />
+          <VictoryAxis
+            label="Vehicle Count"
+            dependentAxis={true}
+            style={{
+              axisLabel: { fontSize: 12 },
+              grid: { stroke: '#efefef' },
+              ticks: { stroke: 'grey', size: 5 },
+              tickLabels: { fontSize: 8, padding: 5 }
+            }}
+          />
+          <VictoryGroup offset={0} style={{ data: { width: 15 } }}>
+            <VictoryStack colorScale={'red'}>
+              {this.props.data.map((data, index) => {
+                return <VictoryBar key={index} data={data} />;
+              })}
+            </VictoryStack>
+          </VictoryGroup>
+        </VictoryChart>
+      </div>
+    );
+  }
+}
+
+export default StackedBarGraph;
