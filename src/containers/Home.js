d95d0b3e3ec9a93cdd74aeb9c4b9976c4745c94a
alex <am3081@gmail.com>
2017-09-12 11:47:49 -0400

    homepage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -22,91 +22,80 @@ export default class Home extends Component {
               style={{ height: 1092 }}
             >
               <div className="col-xl-4 col-sm-6 b-l b-t feature-cell">
-                <div className="feature-icon">
-                  <i className="icon-map" />
-                </div>
-                <h6 className="feature-title" />
-                <div className="feature-text">
-                  Gone studies to titles have audiences of and concepts was
-                  motivator, the this more picture a to we ever the taken have
-                  brilliant. Explain on woman concept readiness way.
-                </div>
+                <Link to="/map">
+                  <div className="feature-icon">
+                    <i className="icon-map" />
+                  </div>
+                  <h6 className="feature-title">Interactive Map</h6>
+                  <div className="feature-text">
+                    Explore counts and completeness through an interactive map.
+                    Visualize data and meta data coverage for all traffic
+                    counts.
+                  </div>
+                </Link>
               </div>
               <div className="col-xl-4 col-sm-6 b-r b-l b-t feature-cell">
-                <div className="feature-icon">
-                  <i className="os-icon os-icon-fingerprint" />
-                </div>
-                <h6 className="feature-title">Unique Design</h6>
-                <div className="feature-text">
-                  Gone studies to titles have audiences of and concepts was
-                  motivator, the this more picture a to we ever the taken have
-                  brilliant. Explain on woman concept readiness way.
-                </div>
+                <Link to="/regions">
+                  <div className="feature-icon">
+                    <i className="icon-graph" />
+                  </div>
+                  <h6 className="feature-title">Short Counts</h6>
+                  <div className="feature-text">
+                    View short counts data and collection meta data.
+                  </div>
+                </Link>
               </div>
               <div className="col-xl-4 col-sm-6 b-r b-l feature-cell">
                 <div className="feature-icon">
-                  <i className="os-icon os-icon-fingerprint" />
+                  <i className="icon-chart" />
                 </div>
-                <h6 className="feature-title">Unique Design</h6>
+                <h6 className="feature-title">Continuous Counts</h6>
                 <div className="feature-text">
-                  Gone studies to titles have audiences of and concepts was
-                  motivator, the this more picture a to we ever the taken have
-                  brilliant. Explain on woman concept readiness way.
+                  Analyze continuous counts data and collection meta data
                 </div>
               </div>
               <div className="col-xl-4 col-sm-6 b-r feature-cell">
                 <div className="feature-icon">
-                  <i className="os-icon os-icon-mail-14" />
+                  <i className="icon-book-open" />
                 </div>
-                <h6 className="feature-title">Mail and Chat Layouts</h6>
+                <h6 className="feature-title">Reporting Tools</h6>
                 <div className="feature-text">
-                  Gone studies to titles have audiences of and concepts was
-                  motivator, the this more picture a to we ever the taken have
-                  brilliant. Explain on woman concept readiness way.
+                  Generate and view report templates. Design and share new
+                  reports.
+                </div>
+              </div>
+              <div className="col-xl-4 col-sm-6 b-r b-l feature-cell">
+                <div className="feature-icon">
+                  <i className="os-icon os-icon-user-male-circle2" />
+                </div>
+                <h6 className="feature-title">User Management</h6>
+                <div className="feature-text">
+                  Easily invite and create users. Grant or restrict access to
+                  data products.
+                </div>
+              </div>
+              <div className="col-xl-4 col-sm-6 b-r feature-cell">
+                <div className="feature-icon">
+                  <i className="os-icon os-icon-tasks-checked" />
+                </div>
+                <h6 className="feature-title">Hand Held Field App</h6>
+                <div className="feature-text">
+                  {' '}
+                  Upload counts with regulation field sheets, get location
+                  coordinates from your mobile device, automatically look up and
+                  fill in meta data fields and uplaod pictures from your mobile
+                  camera.
                 </div>
               </div>
               <div className="col-xl-4 col-sm-6 b-l feature-cell">
                 <div className="feature-icon">
-                  <i className="os-icon os-icon-ui-44" />
+                  <i className="icon-event" />
                 </div>
-                <h6 className="feature-title">All in one solution</h6>
+                <h6 className="feature-title">QA & Counts Management</h6>
                 <div className="feature-text">
-                  Gone studies to titles have audiences of and concepts was
-                  motivator, the this more picture a to we ever the taken have
-                  brilliant. Explain on woman concept readiness way.
-                </div>
-              </div>
-              <div className="col-xl-4 col-sm-6 b-r b-l feature-cell">
-                <div className="feature-icon">
-                  <i className="os-icon os-icon-cv-2" />
-                </div>
-                <h6 className="feature-title">Gorgeous User Profiles</h6>
-                <div className="feature-text">
-                  Gone studies to titles have audiences of and concepts was
-                  motivator, the this more picture a to we ever the taken have
-                  brilliant. Explain on woman concept readiness way.
-                </div>
-              </div>
-              <div className="col-xl-4 col-sm-6 b-r feature-cell">
-                <div className="feature-icon">
-                  <i className="os-icon os-icon-crown" />
-                </div>
-                <h6 className="feature-title">Elite Author</h6>
-                <div className="feature-text">
-                  Gone studies to titles have audiences of and concepts was
-                  motivator, the this more picture a to we ever the taken have
-                  brilliant. Explain on woman concept readiness way.{' '}
-                </div>
-              </div>
-              <div className="col-xl-4 col-sm-6 b-r feature-cell">
-                <div className="feature-icon">
-                  <i className="os-icon os-icon-crown" />
-                </div>
-                <h6 className="feature-title">Elite Author</h6>
-                <div className="feature-text">
-                  Gone studies to titles have audiences of and concepts was
-                  motivator, the this more picture a to we ever the taken have
-                  brilliant. Explain on woman concept readiness way.{' '}
+                  Manage counts submitted from vendors, municipalities and
+                  MPO's. Automatically detect data quality flags. Edit and
+                  approve data for counts system. Generate reports and invoices.{' '}
                 </div>
               </div>
             </div>
